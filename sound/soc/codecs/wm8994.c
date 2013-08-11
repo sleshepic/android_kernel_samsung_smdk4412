3b422511e823cfca2c46545928c7b9588e0dce52
ian schlesinger <ian@ian-Aspire-7750G.(none)>
2013-08-11 19:11:59 -0400

    first add of Sound Control

--- sound/soc/codecs/wm8994.c
+++ sound/soc/codecs/wm8994.c
@@ -193,12 +193,28 @@ static int wm8994_volatile(struct snd_soc_codec *codec, unsigned int reg)
 	}
 }
 
+#ifdef CONFIG_SND_BOEFFLA
+#include "boeffla_sound.h"
+#endif
+
+#ifdef CONFIG_SND_WOLFSON_SOUND_CONTROL
+#include "sound_control.h"
+#endif
+
 static int wm8994_write(struct snd_soc_codec *codec, unsigned int reg,
 	unsigned int value)
 {
 	int ret;
 
 	BUG_ON(reg > WM8994_MAX_REGISTER);
+	
+#ifdef CONFIG_SND_BOEFFLA
+	value = Boeffla_sound_hook_wm8994_write(reg, value);
+#endif
+	
+#ifdef CONFIG_SND_WOLFSON_SOUND_CONTROL
+	value = sound_control_hook_wm8994_write(reg, value);
+#endif
 
 	if (!wm8994_volatile(codec, reg)) {
 		ret = snd_soc_cache_write(codec, reg, value);
@@ -4213,6 +4229,14 @@ static int wm8994_codec_probe(struct snd_soc_codec *codec)
 					ARRAY_SIZE(wm8958_intercon));
 		break;
 	}
+	
+#ifdef CONFIG_SND_BOEFFLA
+	Boeffla_sound_hook_wm8994_pcm_probe(codec);
+#endif
+	
+#ifdef CONFIG_SND_WOLFSON_SOUND_CONTROL
+	sound_control_hook_wm8994_pcm_probe(codec);
+#endif
 
 	return 0;
 
